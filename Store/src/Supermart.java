7a323c13626f6199cab6a74cfbc640b78e94f338
thommoboy <lambofgod2009@live.com.au>
2018-05-26 00:44:24 +1000

    Excessive Cleaning and Commenting
    An Excessive amount of cleaning and commenting was completed

--- Store/src/Supermart.java
+++ Store/src/Supermart.java
@@ -7,7 +7,9 @@ import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -15,136 +17,111 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileSystemView;
 import javax.swing.table.DefaultTableModel;
 
 import SuperMarket.Store;
 
-public class Supermart extends JFrame{
-
-	/**
-	 * 
-	 */
+public class Supermart extends JFrame
+{
 	private static final long serialVersionUID = 1L;
-	private DefaultTableModel model;
-	private JTable table;
 	private Produce.Stock stock = new Produce.Stock();
 	private SuperMarket.Store store = Store.getInstance();
 	
-	public Supermart() {
-		
-	}
+	//UI Items:
+	private DefaultTableModel tableModel;
+	private DefaultTableModel model;
+	private Container uiContainer;
+	private JTextField capitalValue;
+	private JTable table;
+	private JLabel capitalLabel;
+	private JPanel inputPanel;
 
-	public static void main(String[] args) {
+	//Menu Items / Bars:
+	private JMenuItem loadMenuItemProperties;
+	private JMenuItem loadMenuManifests;
+	private JMenuItem loadMenuSalesLogs;
+	private JMenuItem exitMenuItem;
+	private JMenuBar menubar;
+	private JMenu file;
+	
+	//File System / JFileChooser:
+	private JFileChooser loadPropertiesFileChooser;
+	private int loadPropertiesReturnValue;
+	private File selectedFile;
+	
+	public Supermart() {}
+
+	public static void main(String[] args)
+	{
 		Supermart supermart = new Supermart();
 		supermart.RenderUI();
 		supermart.createMenuBar();
 	}
 	
+	/*
+	 * To Add to a Table Model: model.addRow(item);
+	 * To Remove from a Table Model: model.removeRow(table.getSelectedRow());
+	 */
 	public void RenderUI()
 	{
-	    model = new DefaultTableModel();
-	    model.addColumn("Name");
-	    model.addColumn("Quantity");
-	    model.addColumn("Manufacturing Cost");
-	    model.addColumn("Sell Price");
-	    model.addColumn("Re-Order Point");
-	    model.addColumn("Re-Order Amount");
-	    model.addColumn("Temperature");
-	    
-	   
-	    table = new JTable(model);
-
-	    JButton addButton = new JButton("Add Item");
-	    JPanel inputPanel = new JPanel();
-	    JTextField t1;
-		t1=new JTextField(store.getCapital());
-		t1.setBounds(50,100, 200,30);
-		inputPanel.add(t1);
-	    
-	    addButton.addActionListener(new ActionListener() {
-
-	      public void actionPerformed(ActionEvent event) {
-
-	    	  System.out.println("Entered AddItem ");
-	    	
-	    	  if(stock.getObjectAmount() > 0) {
-	    		  System.out.println("Entered if loop");
-			      for(int i = 0; i < stock.getObjectAmount(); i++) {
-			    	  System.out.println(stock.getObjectAmount());
-			    	  String[] item = { "", "", "", "", "", "", "" };
-			    	  item = stock.showInventory(i);
-				      model.addRow(item);
-			      }
-			      t1.setText(store.getCapital());
-	    	  }
-
-	    	  	
-	      }
-	    });
+		tableModel = new DefaultTableModel();
+		tableModel.addColumn("Name");
+		tableModel.addColumn("Quantity");
+		tableModel.addColumn("Manufacturing Cost");
+		tableModel.addColumn("Sell Price");
+		tableModel.addColumn("Re-Order Point");
+		tableModel.addColumn("Re-Order Amount");
+		tableModel.addColumn("Temperature");
 	     
-	    JButton removeButton = new JButton("Remove Selected Item");
+	    table = new JTable(tableModel);
 
-	    removeButton.addActionListener(new ActionListener() {
-
-	      public void actionPerformed(ActionEvent event) {
-	        model.removeRow(table.getSelectedRow());
-	      }
-	    });
-
-
-	   
-
-		    
-	    inputPanel.add(addButton);
-	    inputPanel.add(removeButton);
+	    inputPanel = new JPanel();
+	    capitalLabel = new JLabel("Store Capital");
+	    capitalValue = new JTextField(store.getCapital());
 	    
+	    capitalValue.setBounds(100,100, 200,30);
 	    
-
-	    Container container = getContentPane();
-	    container.add(new JScrollPane(table), BorderLayout.CENTER);
-	    container.add(inputPanel, BorderLayout.NORTH);
+		inputPanel.add(capitalLabel);
+		inputPanel.add(capitalValue);
+	    
+		uiContainer = getContentPane();
+		uiContainer.add(new JScrollPane(table), BorderLayout.CENTER);
+		uiContainer.add(inputPanel, BorderLayout.NORTH);
 
 	    setDefaultCloseOperation(EXIT_ON_CLOSE);
 	    setSize(500, 300);
 	    setVisible(true);
+	    setTitle("SuperMarket");
 	}
 	
-	private void createMenuBar() {
-
-        JMenuBar menubar = new JMenuBar();
-        ImageIcon exitIcon = new ImageIcon("src/main/resources/exit.png");
-
-        JMenu file = new JMenu("File");
-        file.setMnemonic(KeyEvent.VK_F);
+	private void createMenuBar()
+	{
+		//Create a New MenuBar
+		menubar = new JMenuBar();
+		
+		//Initialize the "File" Menu Item.
+		file = new JMenu("File");
+        file.setMnemonic(KeyEvent.VK_F); //Give it a shortcut key
 
         // Menu Items:
-        JMenuItem loadMenuItemProperties = new JMenuItem("Load Item Properties"); 
-        JMenuItem loadMenuManifests = new JMenuItem("Export Manifest"); 
-        JMenuItem loadMenuSalesLogs = new JMenuItem("Load Sales Log"); 
-        JMenuItem eMenuItem = new JMenuItem("Exit", exitIcon);
-        
-        // Menu Item Settings:
+        loadMenuItemProperties = new JMenuItem("Load Item Properties"); 
+        loadMenuManifests = new JMenuItem("Export Manifest"); 
+        loadMenuSalesLogs = new JMenuItem("Load Sales Log"); 
+        exitMenuItem = new JMenuItem("Exit");
         
         //Load Properties Menu:
         loadMenuItemProperties.setToolTipText("Load in an Item Properties File");
         loadMenuItemProperties.addActionListener((ActionEvent event) -> {
-        	System.out.println("Entered item_properties");
-        	File createfile = new File("src\\CSV's\\item_properties.csv");
-        	stock.initialise(createfile);
         	
-        	System.out.println("Entered AddItem ");
-	    	  if(stock.getObjectAmount() > 0) {
-	    		  System.out.println("Entered if loop");
-			      for(int i = 0; i < stock.getObjectAmount(); i++) {
-			    	  System.out.println(stock.getObjectAmount());
-			    	  String[] item = { "", "", "", "", "", "", "" };
-			    	  item = stock.showInventory(i);
-				      model.addRow(item);
-			      }
-	    	  }
+        	loadPropertiesFileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 
-        	store.getCapital();
+    		loadPropertiesReturnValue = loadPropertiesFileChooser.showOpenDialog(null);
 
+    		if(loadPropertiesReturnValue == JFileChooser.APPROVE_OPTION)
+    		{
+    			LoadItemProperties();
+    		}
         });
         
         //Load Manifests Menu:
@@ -160,17 +137,17 @@ public class Supermart extends JFrame{
         });
         
         //Exit Menu:
-        eMenuItem.setMnemonic(KeyEvent.VK_E);
-        eMenuItem.setToolTipText("Exit application");
+        exitMenuItem.setMnemonic(KeyEvent.VK_E);
+        exitMenuItem.setToolTipText("Exit application");
         
-        eMenuItem.addActionListener((ActionEvent event) -> {
+        exitMenuItem.addActionListener((ActionEvent event) -> {
             System.exit(0);
         });
 
         file.add(loadMenuItemProperties);
         file.add(loadMenuManifests);
         file.add(loadMenuSalesLogs);
-        file.add(eMenuItem);
+        file.add(exitMenuItem);
 
         menubar.add(file);
 
@@ -179,7 +156,21 @@ public class Supermart extends JFrame{
 	
 	private void LoadItemProperties()
 	{
+		selectedFile = loadPropertiesFileChooser.getSelectedFile();
 		
+		stock.initialise(selectedFile);
+		
+		if(stock.getObjectAmount() > 0)
+		{
+		      for(int i = 0; i < stock.getObjectAmount(); i++)
+		      {
+		    	  System.out.println(stock.getObjectAmount());
+		    	  String[] item = { "", "", "", "", "", "", "" };
+		    	  item = stock.showInventory(i);
+		    	  tableModel.addRow(item);
+		      }
+  	  }
+		capitalValue.setText(store.getCapital());
 	}
 	
 	private void LoadManifests()
@@ -189,7 +180,7 @@ public class Supermart extends JFrame{
 	
 	private void LoadSalesLogs()
 	{
-		
+		stock.salesLog(null);
 	}
 
 

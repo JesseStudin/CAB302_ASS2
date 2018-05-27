02a9568b3b55fbb355354566c799f066bba9a697
JesseStudin <Jesse.studin@hotmail.com>
2018-05-27 22:58:34 +1000

    Finished project
    Everything has been polished apart from capital Sums which are innaccurate due to no cargo restrictions  :-1:

--- Store/src/Trucks/OrdinaryTruck.java
+++ Store/src/Trucks/OrdinaryTruck.java
@@ -1,30 +1,24 @@
-<<<<<<< HEAD
+
 /**
  * @author Jesse Studin, Pierce Thompson
-=======
+*/
 /*
  * OrdinaryTruck.Java
  * 
  * Written by: Jesse Studin and Pierce Thompson.
  * 
  * This file handles the non-refridgerated trucks.
->>>>>>> 948a4f87e649f199f682f9c12a97f98946ef89b5
+
  */
 package Trucks;
 
 public class OrdinaryTruck extends Truck {
 
-<<<<<<< HEAD
-	
-	private int cargoCapacity;
-	//create instance of Stock
 
-	@Override
-=======
-	private int cargoCapacity;										//Create a private integer to store the cargoCapacity.
+	private int cargoCapacity;//Create a private integer to store the cargoCapacity.
 
 	//This Function returns the cost of the Truck being used.
->>>>>>> 948a4f87e649f199f682f9c12a97f98946ef89b5
+	@Override
 	public double truckCost(double cargoTotal) {
 		return 750 + (0.25 * (int)cargoTotal);
 	}
